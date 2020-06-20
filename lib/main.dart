dd1f22111d1d819ecd829d47cd40eebe062ba243
jmayta1984 <Monica$2017>
2020-06-20 07:13:47 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+Future testData() async {
+  Firestore db = Firestore.instance;
+
+  var data = await db.collection('event_details').getDocuments();
+
+  var details = data.documents.toList();
+
+  details.forEach((element) {
+    print(element.documentID);
+  });
+}
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    testData();
+    return MaterialApp(
+      title: 'Events',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: Scaffold(),
+    );
+  }
+}
---
+++ lib/models/event_detail.dart
@@ -0,0 +1,19 @@
+class EventDetail {
+  String id;
+  String _description;
+  String _date;
+  String _startTime;
+  String _endTime;
+  String _speaker;
+  String _isFavorite;
+
+  EventDetail(
+    this.id,
+    this._description,
+    this._date,
+    this._startTime,
+    this._endTime,
+    this._speaker,
+    this._isFavorite,
+  );
+}
